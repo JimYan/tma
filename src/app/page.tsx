9dd70a85e1bed48b77a950f6ad826ebec553fbd3
jimyan <jimyan@tencent.com>
2024-08-04 18:26:26 +0800

    去掉 mainfast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,57 +24,52 @@ export default function Home() {
   //   postEvent("web_app_expand");
   // }, []);
   return (
-    <TonConnectUIProvider manifestUrl="https://tma-jimyans-projects.vercel.app/tonconnect-manifest.json">
-      <List>
-        <Section
-          header="Features"
-          footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-        >
-          <Link href="/ton-connect">
-            <Cell
-              before={
-                <Image
-                  src={tonSvg.src}
-                  style={{ backgroundColor: "#007AFF" }}
-                />
-              }
-              subtitle="Connect your TON wallet"
-            >
-              TON Connect
-            </Cell>
-          </Link>
-        </Section>
-        <Section
-          header="Application Launch Data"
-          footer="These pages help developer to learn more about current launch information"
-        >
-          <Link href="/init-data">
-            <Cell subtitle="User data, chat information, technical data">
-              Init Data
-            </Cell>
-          </Link>
-          <Link href="/launch-params">
-            <Cell subtitle="Platform identifier, Mini Apps version, etc.">
-              Launch Parameters
-            </Cell>
-          </Link>
-          <Link href="/theme-params">
-            <Cell subtitle="Telegram application palette information">
-              Theme Parameters
-            </Cell>
-          </Link>
-        </Section>
-        <Section>
-          <TonConnectButton />
-        </Section>
-
-        <Section>
-          <Cell subtitle="Counter">
-            <p>value:{value}</p>
-            <p>address:{xaddress}</p>
+    <List>
+      <Section
+        header="Features"
+        footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
+      >
+        <Link href="/ton-connect">
+          <Cell
+            before={
+              <Image src={tonSvg.src} style={{ backgroundColor: "#007AFF" }} />
+            }
+            subtitle="Connect your TON wallet"
+          >
+            TON Connect
           </Cell>
-        </Section>
-      </List>
-    </TonConnectUIProvider>
+        </Link>
+      </Section>
+      <Section
+        header="Application Launch Data"
+        footer="These pages help developer to learn more about current launch information"
+      >
+        <Link href="/init-data">
+          <Cell subtitle="User data, chat information, technical data">
+            Init Data
+          </Cell>
+        </Link>
+        <Link href="/launch-params">
+          <Cell subtitle="Platform identifier, Mini Apps version, etc.">
+            Launch Parameters
+          </Cell>
+        </Link>
+        <Link href="/theme-params">
+          <Cell subtitle="Telegram application palette information">
+            Theme Parameters
+          </Cell>
+        </Link>
+      </Section>
+      <Section>
+        <TonConnectButton />
+      </Section>
+
+      <Section>
+        <Cell subtitle="Counter">
+          <p>value:{value}</p>
+          <p>address:{xaddress}</p>
+        </Cell>
+      </Section>
+    </List>
   );
 }
