71a58d62d4e5a1cac16fd8e3c2cfaee50f69ed8d
jimyan <jimyan@tencent.com>
2024-08-04 17:43:04 +0800

    u

--- src/app/my-test/page.tsx
+++
@@ -1,7 +0,0 @@
-"use server";
-
-export default async function ThemeParamsPage() {
-  const x = await fetch("http://127.0.0.1:3000/api/test");
-  const v = await x.json();
-  return <>asdf:{v.name}</>;
-}
